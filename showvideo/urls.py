b450db31e1297d8fbc6c31364f8bff49ecbbc868
Kirill Kazak <Kirill.bar.kazak@gmail.com>
2020-06-21 10:37:52 +0300

    JS comment using get

--- showvideo/urls.py
+++ showvideo/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    # re_path("123/", views.hello),
+    re_path("123/", views.hello),
     re_path("456/", views.world, name="main_page"),
     re_path("comment/(?P<id>\d+)/", views.accept_comment, name="add_comment"),
     re_path("one_video/(?P<id>\d+)/", views.one_video, name="one_video"),
